7b57084e40790a1d0bc4ebc51cef1cedd8a44202
felix.lin <fly_81211@hotmail.com>
2016-09-07 08:55:19 +0800

    Added uuid classes.

---
+++ Collector/Classes/DeviceUID.h
@@ -0,0 +1,13 @@
+//
+//  DeviceUID.h
+//  Pods
+//
+//  Created by LBH on 2016/9/7.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DeviceUID : NSObject
+
+@end
---
+++ Collector/Classes/DeviceUID.m
@@ -0,0 +1,13 @@
+//
+//  DeviceUID.m
+//  Pods
+//
+//  Created by LBH on 2016/9/7.
+//
+//
+
+#import "DeviceUID.h"
+
+@implementation DeviceUID
+
+@end
