df768d2d58667f852088c568ffc371c538848e1e
felix.lin <fly_81211@hotmail.com>
2016-09-17 23:45:08 +0800

    Added device classes.

---
+++ Collector/Classes/DeviceInfo.h
@@ -0,0 +1,13 @@
+//
+//  DeviceInfo.h
+//  Pods
+//
+//  Created by LBH on 2016/9/17.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DeviceInfo : NSObject
+
+@end
---
+++ Collector/Classes/DeviceInfo.m
@@ -0,0 +1,13 @@
+//
+//  DeviceInfo.m
+//  Pods
+//
+//  Created by LBH on 2016/9/17.
+//
+//
+
+#import "DeviceInfo.h"
+
+@implementation DeviceInfo
+
+@end
